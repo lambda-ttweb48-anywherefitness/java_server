c04cc60f50a0a1aa33d3d049f7a8a9cddd76da6f
zempo <zelenkosolomon@gmail.com>
2021-03-01 11:39:26 -0800

    add project files

---
+++ src/main/java/com/lambdaschool/anywherefitness/AnywherefitnessApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.anywherefitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnywherefitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnywherefitnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lambdaschool/anywherefitness/AnywherefitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.anywherefitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnywherefitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
