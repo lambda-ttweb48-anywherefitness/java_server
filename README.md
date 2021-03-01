ec6db1d0ebf0060bb9724ad97c2501dce955c165
zempo <zelenkosolomon@gmail.com>
2021-03-01 12:08:53 -0800

    add readme, configurations

---
+++ README.md
@@ -0,0 +1,3 @@
+# Anywhere Fitness
+
+## 
---
+++ src/main/java/com/lambdaschool/anywherefitness/config/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Contains all the classes related to configurations.
+ * <p>
+ * Also see:<br>
+ * application.properties.
+ *
+ * @author Solomon Zelenko (support@solomonzelenko.dev) with Lambda School unless otherwise noted.
+ */
+package com.lambdaschool.anywherefitness.config;
